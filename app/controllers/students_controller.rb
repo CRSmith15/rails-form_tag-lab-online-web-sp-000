fc55124581b255b4a221006d3cc77bdf535f5f90
Chance Smith <chancesmith97@hotmail.com>
2020-04-08 22:20:51 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,8 +12,8 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = params
-    redirect_to new_student_path(student: @student)
+    session[:form_params] = params.inspect
+      redirect_to new_student_path
   end
 
 
